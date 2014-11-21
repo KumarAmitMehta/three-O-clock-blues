4198ceafe6a67ace9a2c7d2c13df8e1e0d886b32
Kumar Amit Mehta <gmate.amit@gmail.com>
2014-11-21 17:56:11 +0100

    Sample module, showing Linux kernel implementation of linked-list implementation.
    Signed-off-by: Kumar Amit Mehta <gmate.amit@gmail.com>

---
+++ list/simple_list.c
@@ -0,0 +1,100 @@
+/*
+ * Trivial example, showing usage of data structure, more specifically
+ * link-list implementation mechanism provided by Linux kernel.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kumar Amit Mehta");
+
+struct identity {
+	char name[20];
+	int id;
+	bool busy;
+	struct list_head id_list;
+};
+
+static struct list_head superhero_list;
+
+static int superhero_create(char *name, int id)
+{
+	struct identity *p;
+
+	p = kmalloc(sizeof(struct identity), GFP_KERNEL);
+	if (!p)
+		return -ENOMEM;
+
+	strcpy(p->name, name);
+	p->id = id;
+	p->busy = false;
+
+	list_add(&p->id_list, &superhero_list);
+	return 0;
+}
+
+static struct identity *superhero_find(int id)
+{
+	struct identity *p;
+
+	list_for_each_entry(p, &superhero_list, id_list) {
+		if (p->id == id)
+			return p;
+	}
+
+	return NULL;
+}
+
+static void superhero_destroy(int id)
+{
+	struct identity *p;
+
+	list_for_each_entry(p, &superhero_list, id_list) {
+		if (p->id == id) {
+			list_del(&p->id_list);
+			kfree(p);
+			break;
+		}
+	}
+
+}
+static int my_init(void)
+{
+	struct identity *temp;
+
+	INIT_LIST_HEAD(&superhero_list);
+
+	superhero_create("Superman", 1);
+	superhero_create("Spiderman", 2);
+	superhero_create("Batman", 3);
+	superhero_create("Sherlock", 10);
+
+	// Spiderman, are you still there?
+	temp = superhero_find(2);
+	pr_debug("id 2 = %s\n", temp->name);
+
+	temp = superhero_find(42);
+	if (temp == NULL)
+		pr_debug("id 42 not found\n");
+
+	// Aah! Me the mIghty
+	superhero_destroy(2);
+	superhero_destroy(1);
+	superhero_destroy(3);
+	superhero_destroy(10);
+	superhero_destroy(42);
+	superhero_destroy(10);
+
+	return 0;
+}
+
+static void my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
